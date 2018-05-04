74ed4f7571494c6949a2ba429255bcd0a736632d
ennns <muhammetakdereli@std.sehir.edu.tr>
2018-05-04 17:30:26 +0300

    refresh1

--- DjangoIkinci/cpp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Brand,Series,Model,UserPreference
-# Register your models here.
-
-admin.site.register(Brand)
-admin.site.register(Series)
-admin.site.register(Model)
-admin.site.register(UserPreference)
--- DjangoIkinci/cpp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CppConfig(AppConfig):
-    name = 'cpp'
