da0cdd1513549c8176287be45dbb2916580409c7
enesakdereli <enesakdereli@hotmail.com>
2018-05-28 19:47:04 +0300

    Car detail inputs are changed to radiobutton

--- cpp/forms.py
+++ cpp/forms.py
@@ -2,31 +2,29 @@ from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CarProperties, Brand, Model, Series
-from django.forms import ModelForm
+from django.forms import ModelForm, RadioSelect
 from django.core import serializers
 import inspect
+
+CAR_DETAIL_CHOICES = [(0, 'Unavailable'), (1, 'Available'), (2, 'Unimportant')]
+
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
-
 
 class PricePredictionForm(ModelForm):
     class Meta:
         model = CarProperties
         fields=[f.name for f in model._meta.get_fields()][1:]#or [1:] ?
-        print(fields)
-        #fields = ('brand', 'series', 'model', 'year', 'power', 'fuel_type', 'gear_type', 'case_type', 'owner_type','exchange_status', 'color','abc','abs')
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
+        radio_fields = [f.name for f in CarProperties._meta.get_fields()][13:]
+        for field in radio_fields:
+            self.fields[field].widget = forms.RadioSelect(choices=CAR_DETAIL_CHOICES)
         self.fields['series'].queryset = Series.objects.none()
         self.fields['model'].queryset = Model.objects.none()
-        """hidden_fields = ['case_type','owner_type','exchange_status','color']
-        for i in hidden_fields:
-            self.fields[i].widget = forms.HiddenInput(attrs = { 'id' : 'hidden'})"""
         if 'brand' in self.data:
             try:
                 brand_id = int(self.data.get('brand'))
